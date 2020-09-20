3e70329dd2330ed48f555445ed1bac96bc1ebf7f
Shounak <shounakcy@gmail.com>
2020-09-20 19:29:45 +0200

    Machine Learning Lab 1

--- dectrees/python/entropy.py
+++
@@ -1,6 +0,0 @@
-import monkdata as m
-from dtree import entropy
-
-print(" Entropy of Monk 1 :", entropy(m.monk1))
-print(" Entropy of Monk 2 :", entropy(m.monk2))
-print(" Entropy of Monk 3 :", entropy(m.monk3))
